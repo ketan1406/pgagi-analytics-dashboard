418dba3a29b88a9552ff74568515d03c5b9f9fac
Ketan <sainiketan99@gmail.com>
2025-01-15 10:43:20 +0530

    Added Sidebar and Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-// pages/index.tsx
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/Layouts/Layout';
 
 export default function HomePage() {
   return (
     <Layout>
       <h1 className="text-3xl font-bold">Welcome to PGAGI Dashboard</h1>
       <p>This is your main landing page.</p>
+
     </Layout>
   );
 }
